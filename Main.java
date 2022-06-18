4dc54674e1241002fd3514eb51fff6c66660aeb6
adama coulibaly <103642624+damiscoulibaly@users.noreply.github.com>
2022-06-18 23:21:26 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.printlnHello"); 
+public class Main 
+    ublic static void main(String[] args) {
+        System.out.println"Hello"); 
     }
 }
 
